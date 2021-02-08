778f6a7107e3cdc4f9adabe9aee56536e95122b3
XuehaiPan <XuehaiPan@pku.edu.cn>
2021-02-08 13:35:13 +0800

    chore: change to beta version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     ],
     python_requires='>=3.5, <4',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
