bc145491d85635c9503b416c372f47315c4747e1
XuehaiPan <XuehaiPan@pku.edu.cn>
2021-02-13 18:26:29 +0800

    chore: dump version to v0.2.2

--- nvitop/__init__.py
+++ nvitop/__init__.py
@@ -3,7 +3,7 @@
 
 """An interactive NVIDIA-GPU process viewer."""
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __license__ = 'GPLv3'
 __author__ = __maintainer__ = 'Xuehai Pan'
 __email__ = 'XuehaiPan@pku.edu.cn'
