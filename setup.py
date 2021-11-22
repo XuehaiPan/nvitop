eb1325fd35fac7f197681d3a4eea38979073ff70
XuehaiPan <XuehaiPan@pku.edu.cn>
2021-11-22 21:35:24 +0800

    chore(setup): prefer setup.cfg to setup.py for packaging

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 import os
 import sys
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 try:
     from nvitop import version
@@ -14,61 +14,10 @@ except ImportError:
     import version
 
 
-extra_description = {}
-try:
-    with open('README.md', mode='rt', encoding='UTF-8') as doc:
-        extra_description['long_description'] = doc.read()
-        extra_description['long_description_content_type'] = 'text/markdown'
-except (OSError, UnicodeError):
-    pass
-
 setup(
     name='nvitop',
     version=version.__version__,
     description=version.__doc__,
-    **extra_description,
-    license=version.__license__,
     author=version.__author__,
     author_email=version.__email__,
-    url="https://github.com/XuehaiPan/nvitop",
-    packages=find_packages(include=['nvitop', 'nvitop.*']),
-    entry_points={'console_scripts': ['nvitop=nvitop.cli:main']},
-    install_requires=[
-        'nvidia-ml-py == 11.450.51',
-        'psutil >= 5.5.0',
-        'cachetools >= 1.0.1',
-        'termcolor >= 1.0.0',
-        'colorama >= 0.4.0; platform_system == "Windows"',
-        'windows-curses >= 2.2.0; platform_system == "Windows"',
-    ],
-    python_requires='>= 3.5',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux',
-        'Environment :: GPU',
-        'Environment :: GPU :: NVIDIA CUDA',
-        'Environment :: Console',
-        'Environment :: Console :: Curses',
-        'Intended Audience :: Developers',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: System Administrators',
-        'Topic :: System :: Hardware',
-        'Topic :: System :: Monitoring',
-        'Topic :: System :: Systems Administration',
-        'Topic :: Utilities',
-    ],
-    keywords=['nvidia', 'nvidia-smi', 'NVIDIA', 'NVML', 'CUDA', 'GPU', 'top', 'monitoring'],
-    project_urls={
-        'Bug Reports': 'https://github.com/XuehaiPan/nvitop/issues',
-        'Source': 'https://github.com/XuehaiPan/nvitop',
-    },
 )
