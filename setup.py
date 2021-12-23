1f0dc6982d7bdf281ea4e919f32a71186dfe6fa1
Xuehai Pan <XuehaiPan@pku.edu.cn>
2021-12-23 19:13:24 +0800

    feat(setup): update version string in site-packages library during installation
    Signed-off-by: Xuehai Pan <XuehaiPan@pku.edu.cn>

--- setup.py
+++ setup.py
@@ -2,16 +2,30 @@
 
 # pylint: disable=missing-module-docstring
 
-import os
+import pathlib
 import sys
 
 from setuptools import setup
 
+
+VERSION_FILE = pathlib.Path(__file__).absolute().parent / 'nvitop' / 'version.py'
+
 try:
     from nvitop import version
 except ImportError:
-    sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'nvitop'))
+    sys.path.insert(0, str(VERSION_FILE.parent))
     import version
+
+
+VERSION_CONTENT = None
+if not version.__release__:
+    import re
+
+    VERSION_CONTENT = VERSION_FILE.read_text(encoding='UTF-8')
+    VERSION_FILE.write_text(data=re.sub(r"""__version__\s*=\s*('[^']+'|"[^"]+")""",
+                                        r"__version__ = '{}'".format(version.__version__),
+                                        string=VERSION_CONTENT),
+                            encoding='UTF-8')
 
 
 setup(
@@ -21,3 +35,7 @@ setup(
     author=version.__author__,
     author_email=version.__email__,
 )
+
+
+if VERSION_CONTENT is not None:
+    VERSION_FILE.write_text(data=VERSION_CONTENT, encoding='UTF-8')
