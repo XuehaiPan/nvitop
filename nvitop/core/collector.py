aff3767615f07be354b6e4275595b79aebc7e0bf
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-06-25 22:14:59 +0800

    feat(core/collector): feat log metric min/max
    Signed-off-by: Xuehai Pan <XuehaiPan@pku.edu.cn>

--- nvitop/core/collector.py
+++ nvitop/core/collector.py
@@ -11,7 +11,7 @@ import threading
 import time
 from collections import OrderedDict, defaultdict
 from weakref import WeakSet
-from typing import List, Dict, Iterable, NamedTuple, Hashable, Union, Optional
+from typing import List, Tuple, Dict, Iterable, NamedTuple, Hashable, Union, Optional
 
 from nvitop.core import host
 from nvitop.core.device import Device, CudaDevice
@@ -192,37 +192,43 @@ class ResourceMetricCollector:  # pylint: disable=too-many-instance-attributes
         >>> with collector(tag='<tag>'):
         ...     # Do something
         ...     collector.collect()  # -> Dict[str, float]
+        # key -> '<tag>/<target>/<metric (unit)>/<mean/min/max>'
         {
-            '<tag>/host/cpu_percent (%)': 8.967849777683456,
-            '<tag>/host/memory_percent (%)': 21.5,
-            '<tag>/host/swap_percent (%)': 0.3,
-            '<tag>/host/memory_used (GiB)': 91.0136418208109,
-            '<tag>/host/load_average (%) (1 min)': 10.251427386878328,
-            '<tag>/host/load_average (%) (5 min)': 10.072539414569503,
-            '<tag>/host/load_average (%) (15 min)': 11.91126970422139,
-            '<tag>/cuda:0 (gpu:3)/memory_used (MiB)': 3.875,
-            '<tag>/cuda:0 (gpu:3)/memory_free (MiB)': 11015.562499999998,
-            '<tag>/cuda:0 (gpu:3)/memory_total (MiB)': 11019.437500000002,
-            '<tag>/cuda:0 (gpu:3)/memory_percent (%)': 0.0,
-            '<tag>/cuda:0 (gpu:3)/gpu_utilization (%)': 0.0,
-            '<tag>/cuda:0 (gpu:3)/memory_utilization (%)': 0.0,
-            '<tag>/cuda:0 (gpu:3)/fan_speed (%)': 22.0,
-            '<tag>/cuda:0 (gpu:3)/temperature (℃)': 25.0,
-            '<tag>/cuda:0 (gpu:3)/power_usage (W)': 19.11166264116916,
-            '<tag>/cuda:1 (gpu:2)/memory_used (MiB)': 8878.875,
+            '<tag>/host/cpu_percent (%)/mean': 8.967849777683456,
+            '<tag>/host/cpu_percent (%)/min': 6.1,
+            '<tag>/host/cpu_percent (%)/max': 28.1,
             ...,
-            '<tag>/cuda:2 (gpu:1)/memory_used (MiB)': 8182.875,
+            '<tag>/host/memory_percent (%)/mean': 21.5,
+            '<tag>/host/swap_percent (%)/mean': 0.3,
+            '<tag>/host/memory_used (GiB)/mean': 91.0136418208109,
+            '<tag>/host/load_average (%) (1 min)/mean': 10.251427386878328,
+            '<tag>/host/load_average (%) (5 min)/mean': 10.072539414569503,
+            '<tag>/host/load_average (%) (15 min)/mean': 11.91126970422139,
             ...,
-            '<tag>/cuda:3 (gpu:0)/memory_used (MiB)': 9286.875,
+            '<tag>/cuda:0 (gpu:3)/memory_used (MiB)/mean': 3.875,
+            '<tag>/cuda:0 (gpu:3)/memory_free (MiB)/mean': 11015.562499999998,
+            '<tag>/cuda:0 (gpu:3)/memory_total (MiB)/mean': 11019.437500000002,
+            '<tag>/cuda:0 (gpu:3)/memory_percent (%)/mean': 0.0,
+            '<tag>/cuda:0 (gpu:3)/gpu_utilization (%)/mean': 0.0,
+            '<tag>/cuda:0 (gpu:3)/memory_utilization (%)/mean': 0.0,
+            '<tag>/cuda:0 (gpu:3)/fan_speed (%)/mean': 22.0,
+            '<tag>/cuda:0 (gpu:3)/temperature (℃)/mean': 25.0,
+            '<tag>/cuda:0 (gpu:3)/power_usage (W)/mean': 19.11166264116916,
             ...,
-            '<tag>/pid:12345/cuda:1 (gpu:4)/cpu_percent (%)': 151.34342772112265,
-            '<tag>/pid:12345/cuda:1 (gpu:4)/host_memory (MiB)': 44749.72373447514,
-            '<tag>/pid:12345/cuda:1 (gpu:4)/host_memory_percent (%)': 8.675082352111717,
-            '<tag>/pid:12345/cuda:1 (gpu:4)/running_time (min)': 336.23803206741576,
-            '<tag>/pid:12345/cuda:1 (gpu:4)/gpu_memory (MiB)': 8861.0,
-            '<tag>/pid:12345/cuda:1 (gpu:4)/gpu_memory_percent (%)': 80.4,
-            '<tag>/pid:12345/cuda:1 (gpu:4)/gpu_memory_utilization (%)': 6.711118172407917,
-            '<tag>/pid:12345/cuda:1 (gpu:4)/gpu_sm_utilization (%)': 48.23283397736476,
+            '<tag>/cuda:1 (gpu:2)/memory_used (MiB)/mean': 8878.875,
+            ...,
+            '<tag>/cuda:2 (gpu:1)/memory_used (MiB)/mean': 8182.875,
+            ...,
+            '<tag>/cuda:3 (gpu:0)/memory_used (MiB)/mean': 9286.875,
+            ...,
+            '<tag>/pid:12345/cuda:1 (gpu:4)/cpu_percent (%)/mean': 151.34342772112265,
+            '<tag>/pid:12345/cuda:1 (gpu:4)/host_memory (MiB)/mean': 44749.72373447514,
+            '<tag>/pid:12345/cuda:1 (gpu:4)/host_memory_percent (%)/mean': 8.675082352111717,
+            '<tag>/pid:12345/cuda:1 (gpu:4)/running_time (min)/mean': 336.23803206741576,
+            '<tag>/pid:12345/cuda:1 (gpu:4)/gpu_memory (MiB)/mean': 8861.0,
+            '<tag>/pid:12345/cuda:1 (gpu:4)/gpu_memory_percent (%)/mean': 80.4,
+            '<tag>/pid:12345/cuda:1 (gpu:4)/gpu_memory_utilization (%)/mean': 6.711118172407917,
+            '<tag>/pid:12345/cuda:1 (gpu:4)/gpu_sm_utilization (%)/mean': 48.23283397736476,
             ...,
             '<tag>/duration (s)': 7.247399162035435,
             '<tag>/timestamp': 1655909466.9981883
@@ -309,7 +315,7 @@ class ResourceMetricCollector:  # pylint: disable=too-many-instance-attributes
         self._daemon = threading.Thread(name='gpu_metric_collector_daemon', target=self._target, daemon=True)
         self._daemon_running = threading.Event()
 
-    def activate(self, tag: str) -> None:
+    def activate(self, tag: str) -> 'ResourceMetricCollector':
         """Start a new metric collection with the given tag.
 
         Args:
@@ -344,9 +350,11 @@ class ResourceMetricCollector:  # pylint: disable=too-many-instance-attributes
         except RuntimeError:
             pass
 
+        return self
+
     start = activate
 
-    def deactivate(self, tag: Optional[str] = None) -> None:
+    def deactivate(self, tag: Optional[str] = None) -> 'ResourceMetricCollector':
         """Stop the current collection with the given tag and remove all sub-tags.
         If the tag is not specified, deactivate the current active collection.
         For nested collections, the sub-collections will be deactivated as well.
@@ -358,7 +366,7 @@ class ResourceMetricCollector:  # pylint: disable=too-many-instance-attributes
                     raise RuntimeError(
                         'Resource metric collector has not been not started yet.'
                     )
-                return
+                return self
 
             if tag is None:
                 tag = self._metric_buffer.tag
@@ -377,6 +385,8 @@ class ResourceMetricCollector:  # pylint: disable=too-many-instance-attributes
 
             if self._metric_buffer is None:
                 self._daemon_running.clear()
+
+        return self
 
     stop = deactivate
 
@@ -471,7 +481,7 @@ class ResourceMetricCollector:  # pylint: disable=too-many-instance-attributes
             return self._metric_buffer.collect()
 
     def __del__(self) -> None:
-        self.stop()
+        self._daemon_running.clear()
 
     def take_snapshots(self) -> SnapshotResult:  # pylint: disable=missing-function-docstring,too-many-branches,too-many-locals
         if len(self.root_pids) > 0:
@@ -568,12 +578,12 @@ class _MetricBuffer:  # pylint: disable=missing-class-docstring,missing-function
 
         self.tag = tag
         if self.prev is not None:
-            self.key_prefix = '{}/{}'.format(self.prev.tag, self.tag)
+            self.key_prefix = '{}/{}'.format(self.prev.key_prefix, self.tag)
         else:
             self.key_prefix = self.tag
 
         self.last_timestamp = self.start_timestamp = timer()
-        self.buffer = defaultdict(lambda: _MeanValueMaintainer(self.last_timestamp))
+        self.buffer = defaultdict(lambda: _StatisticsMaintainer(self.last_timestamp))
 
         self.len = 0
 
@@ -595,8 +605,11 @@ class _MetricBuffer:  # pylint: disable=missing-class-docstring,missing-function
         self.len = 0
 
     def collect(self) -> Dict[str, float]:
-        metrics = {'{}/{}'.format(self.key_prefix, key): maintainer.mean()
-                   for key, maintainer in self.buffer.items()}
+        metrics = {
+            '{}/{}/{}'.format(self.key_prefix, key, name): value
+            for key, stats in self.buffer.items()
+            for name, value in stats.items()
+        }
         metrics['{}/duration (s)'.format(self.key_prefix)] = timer() - self.start_timestamp
         metrics['{}/timestamp'.format(self.key_prefix)] = time.time()
         return metrics
@@ -605,12 +618,14 @@ class _MetricBuffer:  # pylint: disable=missing-class-docstring,missing-function
         return self.len
 
 
-class _MeanValueMaintainer:  # pylint: disable=missing-class-docstring,missing-function-docstring
+class _StatisticsMaintainer:  # pylint: disable=missing-class-docstring,missing-function-docstring
     def __init__(self, timestamp: float) -> None:
         self.start_timestamp = timestamp
+        self.last_timestamp = None
         self.integral = None
         self.last_value = None
-        self.last_timestamp = None
+        self.min_value = None
+        self.max_value = None
         self.has_nan = False
 
     def add(self, value: float, timestamp: Optional[float] = None) -> None:
@@ -623,10 +638,12 @@ class _MeanValueMaintainer:  # pylint: disable=missing-class-docstring,missing-f
 
         if self.last_value is None:
             self.integral = value * (timestamp - self.start_timestamp)
-            self.last_value = value
+            self.last_value = self.min_value = self.max_value = value
         else:
             self.integral += (value + self.last_value) * (timestamp - self.last_timestamp) / 2.0
             self.last_value = value
+            self.min_value = min(self.min_value, value)
+            self.max_value = max(self.max_value, value)
 
         self.last_timestamp = timestamp
 
@@ -639,3 +656,18 @@ class _MeanValueMaintainer:  # pylint: disable=missing-class-docstring,missing-f
         timestamp = timer()
         integral = self.integral + self.last_value * (timestamp - self.last_timestamp)
         return integral / (timestamp - self.start_timestamp)
+
+    def min(self) -> float:
+        if self.has_nan and self.min_value is None:
+            return math.nan
+        return self.min_value
+
+    def max(self) -> float:
+        if self.has_nan and self.max_value is None:
+            return math.nan
+        return self.max_value
+
+    def items(self) -> Iterable[Tuple[str, float]]:
+        yield ('mean', self.mean())
+        yield ('min', self.min())
+        yield ('max', self.max())
