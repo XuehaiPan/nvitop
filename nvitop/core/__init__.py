fab7cf85481b64abeb8942b0aa2c2feb69b5a904
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-07-01 02:10:46 +0800

    docs(core/utils): add detailed documentation for utilities
    Signed-off-by: Xuehai Pan <XuehaiPan@pku.edu.cn>

--- nvitop/core/__init__.py
+++ nvitop/core/__init__.py
@@ -1,7 +1,7 @@
 # This file is part of nvitop, the interactive NVIDIA-GPU process viewer.
 # License: GNU GPL version 3.
 
-# pylint: disable=missing-module-docstring
+"""The core APIs of nvitop."""
 
 from nvitop.core import host, utils
 from nvitop.core.libnvml import nvml, nvmlCheckReturn, NVMLError
