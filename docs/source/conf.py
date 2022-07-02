13247776433dfed77f833e1551982ce5e7bb4651
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-07-02 17:05:11 +0800

    docs: resolve sphinx warnings
    Signed-off-by: Xuehai Pan <XuehaiPan@pku.edu.cn>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -135,7 +135,7 @@ napoleon_include_special_with_doc = True
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
 # -- Options for todo extension ----------------------------------------------
 
