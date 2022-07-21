7c7450f99fb50c98f74993149134222b3e760c5c
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-07-21 10:30:51 +0800

    feat(workflows): add wheel tests
    Signed-off-by: Xuehai Pan <XuehaiPan@pku.edu.cn>

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+# To install `nvitop` with specific version of `nvidia-ml-py`, use:
+#
+#   pip install nvidia-ml-py==xx.yyy.zz nvitop
+#
+# or
+#
+#   pip install 'nvitop[pynvml-xx.yyy.zz]'
+#
+
 # pylint: disable=missing-module-docstring
 
 import pathlib
@@ -31,28 +40,19 @@ if not version.__release__:
         encoding='UTF-8',
     )
 
-
-# To install `nvitop` with specific version of `nvidia-ml-py`, use:
-#
-#   pip install nvidia-ml-py==xx.yyy.zz nvitop
-#
-# or
-#
-#   pip install 'nvitop[pynvml-xx.yyy.zz]'
-#
-setup(
-    name='nvitop',
-    version=version.__version__,
-    extras_require={
-        'cuda10': ['nvidia-ml-py == 11.450.51'],
-        **{
-            # The identifier could not start with numbers, add a prefix `pynvml-`
-            'pynvml-{}'.format(pynvml): ['nvidia-ml-py == {}'.format(pynvml)]
-            for pynvml in version.PYNVML_VERSION_CANDIDATES
+try:
+    setup(
+        name='nvitop',
+        version=version.__version__,
+        extras_require={
+            'cuda10': ['nvidia-ml-py == 11.450.51'],
+            **{
+                # The identifier could not start with numbers, add a prefix `pynvml-`
+                'pynvml-{}'.format(pynvml): ['nvidia-ml-py == {}'.format(pynvml)]
+                for pynvml in version.PYNVML_VERSION_CANDIDATES
+            },
         },
-    },
-)
-
-
-if VERSION_CONTENT is not None:
-    VERSION_FILE.write_text(data=VERSION_CONTENT, encoding='UTF-8')
+    )
+finally:
+    if VERSION_CONTENT is not None:
+        VERSION_FILE.write_text(data=VERSION_CONTENT, encoding='UTF-8')
