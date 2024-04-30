29e01369341b0caf50061b0d4128c1954c426659
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-04-30 15:05:24 +0000

    deps(nvidia-ml-py): add `nvidia-ml-py` 12.550.52 to support list

--- nvitop/version.py
+++ nvitop/version.py
@@ -73,6 +73,7 @@ PYNVML_VERSION_CANDIDATES = (
     '12.535.108',
     '12.535.133',
     '12.535.161',
+    '12.550.52',
 )
 """The list of supported ``nvidia-ml-py`` versions.
 See also: `nvidia-ml-py's Release History <https://pypi.org/project/nvidia-ml-py/#history>`_.
